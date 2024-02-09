67b4697c19d898638b91c7ba6f3cfe22a5852e1f
Gloria Lim <glorialimartt@gmail.com>
2024-02-08 19:13:40 -0500

    Fetch data from API instead of of mock data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,26 +4,33 @@ import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
 
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads
+} from "helpers/selectors";
+
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10
+    getValue: getTotalPhotos
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4
+    getValue: getTotalTopics
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng"
+    getValue: getUserWithMostUploads
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza"
+    getValue: getUserWithLeastUploads
   }
 ];
 
@@ -85,7 +92,7 @@ class Dashboard extends Component {
           key={panel.id} 
           id={panel.id}
           label={panel.label} 
-          value={panel.value}
+          value={panel.getValue(this.state)}
           onSelect={event => this.selectPanel(panel.id)}
         />
       ));
